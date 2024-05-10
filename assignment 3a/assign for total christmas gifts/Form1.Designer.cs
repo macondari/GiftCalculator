af5a577646990b717b3a641e13728a281ef75bf2
macondari <145869118+macondari@users.noreply.github.com>
2024-05-10 12:25:52 -0400

    Add files via upload

---
+++ assignment 3a/assign for total christmas gifts/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblOut = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(92, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Christmas Gifts";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.btnStart.Location = new System.Drawing.Point(128, 35);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblOut
+            // 
+            this.lblOut.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblOut.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOut.Location = new System.Drawing.Point(12, 115);
+            this.lblOut.Name = "lblOut";
+            this.lblOut.Size = new System.Drawing.Size(313, 124);
+            this.lblOut.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(346, 262);
+            this.Controls.Add(this.lblOut);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblOut;
+    }
+}
+
---
+++ assignment 3a/assign for total christmas gifts/Form1.cs
@@ -0,0 +1,48 @@
+﻿//Michaela Condari, 2021-04-01, 11:40 am
+//purpose: ask a user to enter how many Christmas gifts they are planning to purchase this year.
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //declare variables
+            int gifts;
+            int price;
+            int total = 0;
+
+            //ask number of gifts
+            gifts = Convert.ToInt16(Microsoft.VisualBasic.Interaction.InputBox("Enter how many gifts did you buy","Enter a number"));
+
+            // display that info
+            lblOut.Text += "you purchased " + gifts + " gifts" + "\n";
+
+            //for loop structure
+
+            for (int i= 1; i != gifts + 1; i++)
+            {
+                price = Convert.ToInt16(Microsoft.VisualBasic.Interaction.InputBox(" What is the price of your gift?","Enter a price"));
+
+                total = total + price;
+
+                lblOut.Text += "The price of gift " + i + " is $" + price + "\n";
+            }
+            
+            lblOut.Text += "The total price of the gifts is $" + total;
+        }
+    }
+}
